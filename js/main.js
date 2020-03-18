31765658c77a49be83f004695eb1bb92258dd68f
pavelgitvasilev <49195284+pavelgitvasilev@users.noreply.github.com>
2020-03-18 23:42:57 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var mySwiper = new Swiper('.swiper-container', {
+ slidesPerView: 1, 
+ loop: true,
+ navigation: {
+ nextEl: '.arrow'
+},
+breakpoints: {
+ 540: {
+   slidesPerView: 2
+ }
+}
+
+});
+
+var menuButton = document.querySelector(".menu-button");
+var menu = document.querySelector ('.header');
+menuButton.addEventListener('click', function () {
+ // console.log ('Клин по кнопке');
+ menuButton.classList.toggle('menu-button-active');
+ menu.classList.toggle('header-active')
+})
